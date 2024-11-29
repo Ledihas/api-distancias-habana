29aecda226bbd8deda8e34ced3ff7f30a50de627
Ledihas <zahidel.pedroso@gmail.com>
2024-11-29 18:24:25 -0500

    Resolviendo Problemas

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ def enviar():
     # Verificar que ambos parámetros estén presentes
     if string1 and string2:
         # Construir el texto a enviar a Wit.ai
-        texto_a_enviar = f"deme la distancia que existe entre los municipios   {string1} y {string2} por carretera"
+        texto_a_enviar = f"deme la distancia que existe entre    {string1} y {string2} por carretera"
 
         # URL de la API de Wit.ai para el procesamiento del texto
         url_post = 'https://api.wit.ai/message'
@@ -39,46 +39,56 @@ def enviar():
         if 'intents' in data and data['intents']:
             intent_name = data['intents'][0]['name']
             if intent_name == "distancia" or intent_name == "get_distancia":
+                coordenadas1 = None
+                coordenadas2 = None
                 
                 DIRECCIONES
 
                 # Obtener los lugares reconocidos (origen y destino)
                 lugares = [entity['value'] for entity in data['entities'].get('wit$location:location', [])]
-
+                contador = 0
                 print("Lugares reconocidos:", lugares) 
 
-                if len(lugares) == 2:
-                    # Intentar obtener la distancia en ambas direcciones
-                    coordenadas1 = DIRECCIONES.get(lugares[0])
-                    coordenadas2 = DIRECCIONES.get(lugares[1])
-                    
-                    x1 = coordenadas1[0]
-                    y1 = coordenadas1[1]
-                    x2 = coordenadas2[0]
-                    y2 = coordenadas2[1]
-                    
-                    x1 = radians(x1)
-                    y1 = radians(y1)
-                    
-                    x2 = radians(x2)
-                    y2 = radians(y2)
-                    
-                    
-                    distancia_km = sqrt(pow((x2-x1),2) + pow((y2-y1),2)) * 6371
-                    
-                    # Convertir la respuesta a número si es posible
-                    try:
-                        distancia_numerica = float(distancia_km)
-                        return {"distancia": distancia_numerica}, 200
-                    except (ValueError, TypeError):
-                        return {"error": "No se pudo convertir la distancia a número"}, 400
 
+                for lugar in lugares:
+                    
+                    if lugar in DIRECCIONES:
+                        if contador == 0:
+                        # Intentar obtener la distancia en ambas direcciones
+                            coordenadas1 = DIRECCIONES[lugar]
+                            contador +=1
+                        elif contador == 1:
+                            coordenadas2 = DIRECCIONES[lugar]
+                    else:
+                        return "No se encontró alguna de las dos direcciones"
+                        
+                    
+                x1 = coordenadas1[0]
+                y1 = coordenadas1[1]
+                x2 = coordenadas2[0]
+                y2 = coordenadas2[1]
+                            
+                x1 = radians(x1)
+                y1 = radians(y1)
+                            
+                x2 = radians(x2)
+                y2 = radians(y2)
+                            
+                            
+                distancia_km = sqrt(pow((x2-x1),2) + pow((y2-y1),2)) * 6371
+                            
+                # Convertir la respuesta a número si es posible
+                try:
+                    distancia_numerica = float(distancia_km)
+                    return {"distancia": distancia_numerica}, 200
+                except (ValueError, TypeError):
+                    return {"error": "No se pudo convertir la distancia a número"}, 400
+
+                
             else:
-                return "Intento no reconocido.", 400
+                return "No se pudo procesar la solicitud.", 400
         else:
-            return "No se pudo procesar la solicitud.", 400
-    else:
-        return 'Faltan parámetros string1 o string2', 400
+            return 'Faltan parámetros string1 o string2', 400
     pass
 
 if __name__ == "__main__":
